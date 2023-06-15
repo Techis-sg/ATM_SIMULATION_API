addbb769071e73b12223affb31a2298a388731f8
Shobhit Gagrani <shobhit.gagrani@tanla.com>
2023-06-16 01:58:09 +0530

    fixed auth backend, removed pycache, updated serializers

--- atm_machine/atm/serializers.py
+++ atm_machine/atm/serializers.py
@@ -1,12 +1,7 @@
 from rest_framework import serializers
 from .models import Card
-
-
-class CardSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Card
-        fields = ['id', 'card_number','pin', 'blocked', 'account']
-        
+from accounts.models import User, Account
+from transactions.models import Transaction
 
 class AuthenticateCard(serializers.ModelSerializer):
     class Meta:
@@ -22,3 +17,66 @@ class TokenSerializer(serializers.Serializer):
 class ObtainTokenSerializer(serializers.Serializer):
     card_number = serializers.CharField()
     pin = serializers.CharField()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+
+class ChangePinSerializer(serializers.Serializer):
+    pin = serializers.CharField(max_length=4, min_length=4)
+
+    def validate_pin(self, value):
+        try:
+            int(value)
+        except ValueError:
+            raise serializers.ValidationError('PIN should contain only numeric digits.')
+        return value
+
+
+class TokenSerializer(serializers.Serializer):
+    cardnumber = serializers.CharField(max_length=16)
+    pin = serializers.CharField(max_length=4)
+
+    def validate(self, attrs):
+        print("\n inside validate ")
+        card_number = attrs.get('cardnumber')
+        pin = attrs.get('pin')
+
+        if len(card_number) != 16:
+            raise serializers.ValidationError('Invalid card number.')
+
+        try:
+            int(card_number)
+        except ValueError:
+            raise serializers.ValidationError('Card number should contain only numeric digits.')
+
+        if len(pin) != 4:
+            raise serializers.ValidationError('Invalid PIN.')
+
+        try:
+            int(pin)
+        except ValueError:
+            raise serializers.ValidationError('PIN should contain only numeric digits.')
+
+        return attrs
--- atm_machine/atm/views.py
+++ atm_machine/atm/views.py
@@ -2,6 +2,20 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Card
 from .serializers import CardSerializer
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Card
+from accounts.models import Account
+from transactions.models import Transaction
+from .serializers import (
+    CardSerializer,
+    AccountSerializer,
+    TransactionSerializer,
+    ChangePinSerializer,
+)
+from atm_machine.custom_auth_backend import JWTAuthentication
 
 
 class CardCreateView(generics.CreateAPIView):
@@ -12,3 +26,75 @@ class CardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [IsAuthenticated]
+
+
+class UserFunctionalityViewSet(viewsets.ViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def balance_inquiry(self, request, pk=None):
+        """
+        Get the balance of the account associated with a card.
+        """
+        card = get_object_or_404(Card, number=pk)
+        account = card.account
+        serializer = AccountSerializer(account)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def cash_withdrawal(self, request, pk=None):
+        """
+        Withdraw cash from the account associated with a card.
+        """
+        card = get_object_or_404(Card, number=pk)
+        account = card.account
+        withdrawal_amount = request.data.get('amount')
+
+        if account.balance >= withdrawal_amount:
+            account.balance -= withdrawal_amount
+            account.save()
+            transaction = Transaction.objects.create(
+                account=account,
+                amount=withdrawal_amount,
+                transaction_type='Withdrawal'
+            )
+            serializer = TransactionSerializer(transaction)
+            return Response(serializer.data)
+        else:
+            return Response({'error': 'Insufficient funds'}, status=400)
+
+    @action(detail=True, methods=['post'])
+    def cash_deposit(self, request, pk=None):
+        """
+        Deposit cash to the account associated with a card.
+        """
+        card = get_object_or_404(Card, number=pk)
+        account = card.account
+        deposit_amount = request.data.get('amount')
+
+        account.balance += deposit_amount
+        account.save()
+        transaction = Transaction.objects.create(
+            account=account,
+            amount=deposit_amount,
+            transaction_type='Deposit'
+        )
+        serializer = TransactionSerializer(transaction)
+        return Response(serializer.data, status=201)
+
+    @action(detail=True, methods=['post'])
+    def change_pin(self, request, pk=None):
+        """
+        Change the PIN associated with a card.
+        """
+        card = get_object_or_404(Card, number=pk)
+        serializer = ChangePinSerializer(data=request.data)
+
+        if serializer.is_valid():
+            new_pin = serializer.validated_data['pin']
+            card.pin = new_pin
+            card.save()
+            return Response({'message': 'PIN changed successfully'})
+        else:
+            return Response(serializer.errors, status=400)
