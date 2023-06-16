4cf34e5b06cf34289cc2ab055f1e56038d781650
Saiprasad Malkapuram <prasad.51415@gmail.com>
2023-06-16 12:34:27 +0530

    removed user feild on transcation serializer

--- atm_machine/transactions/serializers.py
+++ atm_machine/transactions/serializers.py
@@ -5,4 +5,4 @@ from .models import Transaction
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['id', 'user', 'amount', 'timestamp']
+        fields = ['id', 'amount', 'timestamp']
