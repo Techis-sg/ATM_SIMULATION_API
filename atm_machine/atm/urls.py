4f218e1e9bb78a5af61d2390565d5bbcc8a244ac
Saiprasda <prasad.51415@gmail.com>
2023-06-16 18:03:01 +0530

    change pin url added

--- atm_machine/atm/urls.py
+++ atm_machine/atm/urls.py
@@ -11,6 +11,10 @@ cash_withdrawal = UserFunctionalityViewSet.as_view({
 cash_deposit = UserFunctionalityViewSet.as_view({
     'post': 'cash_deposit',
 })
+change_pin = UserFunctionalityViewSet.as_view({
+    'post': 'change_pin',
+})
+
 
 
 
@@ -19,7 +23,9 @@ urlpatterns = [
     path('cards/<int:pk>/', CardRetrieveUpdateDestroyView.as_view(), name='card-retrieve-update-destroy'),
     path('balance_inquiry/', balance_inquiry, name='balance_inquiry'),
     path('cash_withdrawal/', cash_withdrawal, name='cash_withdrawal'),
-        path('cash_deposit/', cash_deposit, name='cash_deposit'),
+    path('cash_deposit/', cash_deposit, name='cash_deposit'),
+    path('change_pin/', change_pin, name='change_pin'),
+
 
 
 
